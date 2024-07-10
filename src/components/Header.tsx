a5b0937c41be372a460e1a62203e6368e9ec96bd
aguijarro1029 <aguijarro@hightelecom.com>
2024-07-09 20:16:45 -0500

    logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-
+import logo from '../assets/Img/Logo_Ut.png'
 const Header: React.FC = () => {
   return (
     <header >
       <nav style={{backgroundColor:'#fd6a01'}} className="navbar navbar-expand-lg">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
+          <img src={logo} height={"90px"} width={"590px"} alt="" />
+          
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
