8c66e25093b62ff885a02604b1c91a863a83395c
saarcos <sebasarcosc@gmail.com>
2024-07-22 14:04:39 -0500

    Ajuste-Colores

--- src/components/TiposTalleres.tsx
+++ src/components/TiposTalleres.tsx
@@ -4,12 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const TiposTalleres = () => {
   return (
     <div className='tipo-talleres'>
-        <h1>NUESTROS TALLERES</h1>
+        <h1 className='talleres-title'>NUESTROS TALLERES</h1>
         <div className="line"></div>
         <p style={{justifyContent:'center', textAlign:'center', fontSize:'1rem', color:'gray'}}>Ofrecemos clases adicionales en las áreas de madera, mecánica y gastronomía.</p>
         <div className="first-des">
             <div className="des-text">
-                <h2>Gastronomía <FontAwesomeIcon icon={faUtensils} /></h2>
+                <h2 className='taller-subtitle'>Gastronomía <FontAwesomeIcon icon={faUtensils} /></h2>
                 <p>Sumérgete en el arte culinario con nuestros talleres de gastronomía, aprendiendo técnicas avanzadas de cocina. Perfecciona tus habilidades y sorprende a tus seres queridos con tus creaciones.</p>
             </div>
             <div className="image">
@@ -19,7 +19,7 @@ const TiposTalleres = () => {
         </div>
         <div className="first-des-reverse">
             <div className="des-text">
-                <h2>Mecánica <FontAwesomeIcon icon={faCogs} /></h2>
+                <h2 className='taller-subtitle'>Mecánica <FontAwesomeIcon icon={faCogs} /></h2>
                 <p>Descubre los secretos de la mecánica y el funcionamiento de las máquinas. Aprende desde el mantenimiento básico hasta la reparación avanzada, y desarrolla una comprensión profunda de cómo funcionan los sistemas mecánicos.</p>
             </div>
             <div className="image">
@@ -29,7 +29,7 @@ const TiposTalleres = () => {
         </div>
         <div className="first-des">
             <div className="des-text">
-                <h2>Carpintería <FontAwesomeIcon icon={faHammer} /></h2>
+                <h2 className='taller-subtitle'>Carpintería <FontAwesomeIcon icon={faHammer} /></h2>
                 <p>Desarrolla tus habilidades en carpintería y aprende a trabajar con madera. Desde el diseño y planificación hasta la construcción de muebles y estructuras, nuestros talleres te enseñarán las técnicas esenciales para crear proyectos de madera duraderos y atractivos.</p>
             </div>
             <div className="image">
