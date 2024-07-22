d431d7e8925a5e74d4b1823b531da1710c414747
Tulio162 <167026545+Tulio162@users.noreply.github.com>
2024-07-22 10:59:04 -0500

    Ajuste Historia

--- src/Pages/Historia.tsx
+++ src/Pages/Historia.tsx
@@ -1,4 +1,3 @@
-
 import '../css/inicio.css';
 
 const Historia = () => {
@@ -11,6 +10,11 @@ const Historia = () => {
             alt="Historia de la escuela" 
             className="full-width-image"
           />
+          <img 
+            src="http://adolfo-kolping.de/wordpress/wp-content/gallery/rundgang/rundgang02.jpg" 
+            alt="Historia de la escuela" 
+            className="full-width-image small-image"
+          />
         </div>
         <div className="text-content">
           <div className="history">
@@ -20,8 +24,14 @@ const Historia = () => {
             </p>
             <p>
               ¿Pero cómo empezó todo? Me fui en 1994 a Ecuador como párroco. … Educación y capacitación son de vida necesaria en una industria globalizada. Por lo tanto, la idea de una escuela técnica en el espíritu del sacerdote Adolph Kolping surgió muy pronto. En mi parroquia de Yaruquíes una joven profesora trabajó en el tiempo, Janeth López, la actual directora. Viene de la aldea y conoce las necesidades y preocupaciones de la gente de esta región. Janeth fue inmediatamente entusiasmado con mi idea de una escuela técnica y quería empezar. Así que alquilamos habitaciones en una antigua escuela en Yaruquíes para la tarde y ella sola enseñó. Para las clases prácticas construí dos galpones y contractamos un carpintero y un mecánico que fueron por hora. Juntos hemos desarrollado la escuela, hemos construido sala por sala en un sitio, que conseguimos en el año 2000 con contrato enfitéutico. …
+            </p>
+            <p>
               Después tuvimos que luchar para la aprobación, para que los estudiantes pudieran recibir un título reconocido. Porque era un colegio artesanal y secundaria – el primero en esta forma en el país -, tuvimos que obtener la aprobación de 2 ministerios y la Cámara Nacional de Artesanos. El proceso de aprobación tardó casi 2 años. … Para la firma final del Ministro de trabajo, estuvimos 3 días en Quito y hicimos 8 visitas al Ministerio! …
+            </p>
+            <p>
               Si veamos esta hermosa escuela hoy, apenas se puede creer que empezamos con sólo un salón de clases y dos cobertizos improvisados. Todavía recuerdo, que en el primer día dos alumnas mayores trajeron a sus bebés a la escuela. Tenían a nadie que podría proteger a los pequeños en casa y nos rogaron, asistir a las clases con los bebés. …
+            </p>
+            <p>
               Un gran desafío fue y es la diferente mentalidad de la gente en el altiplano andino. Ecuador se encuentra en el cinturón tropical y allí crecen frutas todo el año con riego suficiente. Esto significa que en estos países, la idea de planificación o prevención, como por ejemplo en la formación profesional, es en gran parte desconocida. Además de eso, que durante 500 años había represión y acostumbrados de la determinación ajena, piensan solamente en el día, pero nunca en la mañana o en pasado mañana. …”
             </p>
             <p>
